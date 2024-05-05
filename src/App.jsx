e7adff47e23e2730166b32243f9be7ba3acdd865
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2024-05-05 19:28:58 +0530

    final code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlight from './components/Highlight'
+import Model from './components/Model'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import HowItWorks from './components/HowItWorks'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +15,11 @@ function App() {
     <Navbar/>
     <Hero/>
     <Highlight/>
+    <Model/>
+    <HowItWorks/>
+    <Features/>
+    
+    <Footer/>
     </main>
   )
 }
